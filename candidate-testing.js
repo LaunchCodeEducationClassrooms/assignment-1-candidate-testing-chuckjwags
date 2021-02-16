4ffd35cc5925d53e8874f884d9451af06d7d908f
ChuckJWags <chuckjwags@gmail.com>
2021-02-16 19:57:33 +0000

    Made Grade return explicit percentages instead of correct answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5+3)/2*10=? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -34,16 +34,18 @@ function gradeQuiz(candidateAnswers) {
   }
 
   let grade = 0;
+  let j = 0;
   
   for (let i = 0; i < correctAnswers.length; i ++) {
     if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
-    grade ++;
+    j ++;
   } else {
-    grade += 0;
+    j += 0;
   }
   }
-  console.log(`>>> Overall Grade: ${grade / correctAnswers.length * 100}% (${grade} of 5 responses correct) <<<`);
-    if (grade >= 4) {
+  grade = j/correctAnswers.length*100;
+  console.log(`>>> Overall Grade: ${grade}% (${j} of 5 responses correct) <<<`);
+    if (j >= 4) {
       console.log(`>>> Status: PASSED <<<`);
     } else {
       console.log(`>>> Status: FAILED <<<`);
